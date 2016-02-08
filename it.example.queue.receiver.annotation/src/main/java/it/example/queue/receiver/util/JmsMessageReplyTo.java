4e11207784f883dd8ce9e371c12bfe6c122910a0
BK001687 <s.lorenzi@almaviva.it>
2016-02-08 21:45:14 +0100

    receiver annotation

---
+++ it.example.queue.receiver.annotation/src/main/java/it/example/queue/receiver/util/JmsMessageReplyTo.java
@@ -0,0 +1,30 @@
+package it.example.queue.receiver.util;
+
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JmsMessageReplyTo {
+
+	@Autowired
+	private JmsTemplate jmsTemplate;
+
+	public void send(final Destination dest, final String text) {
+
+		this.jmsTemplate.send(dest, new MessageCreator() {
+			@Override
+			public Message createMessage(Session session) throws JMSException {
+				Message message = session.createTextMessage(text);
+				return message;
+			}
+		});
+	}
+
+}
